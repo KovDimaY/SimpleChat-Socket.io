b306f796df28b16ddcd470825272efd0181f8d96
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-03-21 18:48:02 +0100

    Initialized the project with a basic server and configurations needed to deploy it on Heroku

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const public = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(public));
+
+app.listen(port, () => {
+  console.log(`Server is running on the port ${port}`);
+});
