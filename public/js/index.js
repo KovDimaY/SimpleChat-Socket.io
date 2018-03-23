ffa9a391e08829fc6e4894306e8aa2a3587377f5
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-03-23 19:05:11 +0100

    Implemented autoscroll behavior

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log("Welcome to the chat!");
 });
@@ -14,6 +28,7 @@ socket.on('newMessage', function (message) {
   const template = jQuery('#message').html();
   const html = Mustache.render(template, { from, text, timestamp });
   jQuery('#messages').append(html);
+  scrollToBottom()
 });
 
 socket.on('newLocation', function (message) {
