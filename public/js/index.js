fabedc67106f1b2788375c290f6319bef0f08c70
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2020-10-06 23:55:03 +0200

    DEV-21 applied prettier to the client files of the project

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,22 @@
 const nameInput = jQuery('#name-input');
 const roomInput = jQuery('#room-input');
 
-const trimNames = function() {
+const trimNames = function () {
   const nameValue = nameInput.val().trim();
   const roomValue = roomInput.val().trim();
   nameInput.val(nameValue);
   roomInput.val(roomValue);
 };
 
-const selectAvatar = function(element) {
-  console.log(element)
+const selectAvatar = function (element) {
+  console.log(element);
   const avatarPicture = jQuery(element);
-  const allAvatars = jQuery(".avatar-selection");
+  const allAvatars = jQuery('.avatar-selection');
   for (let i = 0; i < allAvatars.length; i += 1) {
-    jQuery(allAvatars[i]).removeClass("avatar-active");
+    jQuery(allAvatars[i]).removeClass('avatar-active');
   }
-  avatarPicture.addClass("avatar-active");
-  avatarPicture.find('input').prop("checked", true);
+  avatarPicture.addClass('avatar-active');
+  avatarPicture.find('input').prop('checked', true);
 };
 
 nameInput.on('change', trimNames);
